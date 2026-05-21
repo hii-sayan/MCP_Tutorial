e6a6f250c650f298a109aed86fe2d1670f873a9e
hii-sayan <67260880+hii-sayan@users.noreply.github.com>
2026-05-21 23:44:31 +0530

    added MCP with create-user on cursor

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import { McpServer  } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
+import fs from "node:fs/promises";
 
-const server = new McpServer({
-    name: "test",
-    version: "1.0.0",
-    capabilities: {
-        resources: {},
-        tools: {}, 
-        prompts: {},
+const server = new McpServer(
+    {
+        name: "test",
+        version: "1.0.0",
+    },
+    {
+        capabilities: {
+            resources: {},
+            tools: {},
+            prompts: {},
+        },
     }
-})
+)
 
 server.tool("create-user", "Create a nre user in the database", {
     name: z.string(),
@@ -24,8 +29,40 @@ server.tool("create-user", "Create a nre user in the database", {
     idempotentHint: false,
     openWorldHint: true
 }, async (params) => {
-    return {}
+    try {
+        const id = await createUser(params)
+        return {
+            content: [
+                { type: "text", text: `User ${id} created successfully.`}
+            ]
+        }
+    } catch {
+        return {
+            content: [
+                { type: "text", text: "Failed to save user"}
+            ]
+        }
+    }
 })
+
+async function createUser(user: {
+    name:string,
+    email: string,
+    address: string,
+    phone: string
+}){
+    const users = await import("./data/users.json", {
+        with: { type: "json" }
+    }).then(m => m.default) 
+
+    const id = users.length + 1
+
+    users.push({id, ...user})
+    
+    await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2))
+
+
+}
 
 async function main() {
     const transport = new StdioServerTransport()
